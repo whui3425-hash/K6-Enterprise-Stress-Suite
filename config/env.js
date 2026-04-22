1b635f0164df3e0aac4893826300e011fcef56c2
admin <1518483751@qq>
2026-04-22 21:24:25 +0800

    新建测试项目

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@
  * 全局环境配置（可按客户环境调整）
  */
 export const ENV = {
-  BASE_URL: "http://localhost:8080",
+  BASE_URL: "http://192.168.31.101:8088",
   FIXED_CAPTCHA: "123456",
   VUS: 10,
   DURATION: "1m",
